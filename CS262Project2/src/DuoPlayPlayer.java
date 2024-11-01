c0d3972c7b9ee70c9fa58cb9fb49d2b02e7403bc
Attagoat <attadoesmystiv@gmail.com>
2024-11-01 00:46:34 -0500

    started stuff i guess

---
+++ CS262Project2/src/DuoPlayPlayer.java
@@ -0,0 +1,11 @@
+public abstract class DuoPlayPlayer {
+    protected String playerName;
+    DuoPlayPlayer(){
+        playerName = "Default Name";
+
+    }
+
+    DuoPlayPlayer(String playerName){
+        this.playerName = playerName;
+    }
+}
---
+++ CS262Project2/src/Duoplay.java
@@ -0,0 +1,10 @@
+public abstract class Duoplay {
+    protected DuoPlayPlayer player1;
+    protected DuoPlayPlayer player2;
+    protected
+    Duoplay(DuoPlayPlayer player1, DuoPlayPlayer player2){
+
+
+
+    }
+}
